1399551d3f06d269af0c8ee83e754125c714942c
chenjianai <jianaichen@gmail.com>
2018-12-13 15:10:11 +0800

    添加vlc

--- app/src/main/java/com/reomote/carcontroller/MainActivity.java
+++ app/src/main/java/com/reomote/carcontroller/MainActivity.java
@@ -4,12 +4,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.videolan.VlcPlayer;
+
+
 public class MainActivity extends Activity {
+    VlcPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("big","screen:"+getResources().getDisplayMetrics().widthPixels+","+getResources().getDisplayMetrics().heightPixels);
+        Log.d("big", "screen:" + getResources().getDisplayMetrics().widthPixels + "," + getResources().getDisplayMetrics().heightPixels);
+        mPlayer = (VlcPlayer) findViewById(R.id.player);
+    }
+
+    @Override
+    protected void onResume() {
+        mPlayer.setVideoPath("rtsp://13728735758:abcd1234@10.2.0.76:554/stream1");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mPlayer.stop();
     }
 }
